97ee8fa0a1c9b2a50ec49571e9f5d893acead0d3
xixotron <xixotron89@gmail.com>
2025-09-03 00:31:28 +0200

    upload thumbnails to memory

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"net/url"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+const maxMemory = 10 << 20
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +32,54 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse the form data", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse the thumbnail", err)
+		return
+	}
+
+	fileType := header.Header.Get("Content-Type")
+	fileContent, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get the contents of the thumbnail", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't find the video with the provided ID", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You can't modify that video", nil)
+	}
+
+	videoThumbnails[videoID] = thumbnail{
+		mediaType: fileType,
+		data:      fileContent,
+	}
+
+	thumbnailURL := (&url.URL{
+		Scheme: "http",
+		Host:   r.Host,
+		Path:   "/api/thumbnails",
+	}).JoinPath(videoIDString).String()
+
+	video.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update the video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
