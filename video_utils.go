d7e1d8b24ee763441d75d1471d2a07be93bcfbfb
xixotron <xixotron89@gmail.com>
2025-09-04 23:05:29 +0200

    no float calculations, better errors as per examples, split args into groups

--- video_utils.go
+++ video_utils.go
@@ -5,49 +5,52 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"math"
 	"os/exec"
 )
 
 func getVideoAspectRatio(filePath string) (string, error) {
 	const tolerance = 0.01
-	type Streams struct {
+
+	if filePath == "" {
+		return "", errors.New("No file path provided")
+	}
+	cmd := exec.Command("ffprobe",
+		"-v", "error",
+		"-print_format", "json",
+		"-show_streams",
+		filePath,
+	)
+
+	var buff bytes.Buffer
+	cmd.Stdout = &buff
+
+	if err := cmd.Run(); err != nil {
+		return "", fmt.Errorf("ffprobe error: %w", err)
+	}
+
+	var output struct {
 		Streams []struct {
 			Width  int `json:"width"`
 			Height int `json:"height"`
 		} `json:"streams"`
 	}
 
-	if filePath == "" {
-		return "", errors.New("No file path provided")
-	}
-	cmd := exec.Command("ffprobe", "-v", "error", "-print_format", "json", "-show_streams", filePath)
-
-	buff := new(bytes.Buffer)
-	cmd.Stdout = buff
-
-	err := cmd.Run()
-	if err != nil {
-		return "", err
-	}
-
-	output := Streams{}
-	decoder := json.NewDecoder(buff)
+	decoder := json.NewDecoder(&buff)
 	if err := decoder.Decode(&output); err != nil {
-		return "", fmt.Errorf("Error decoding probe result: %w", err)
+		return "", fmt.Errorf("Error decoding ffprobe output: %w", err)
+	}
+
+	if len(output.Streams) == 0 {
+		return "", fmt.Errorf("No video streams found")
 	}
 
 	width := output.Streams[0].Width
 	height := output.Streams[0].Height
 
-	ratio := float64(width) / float64(height)
-	if math.Abs(ratio-16.0/9.0) < tolerance {
+	if width == 16*height/9 {
 		return "16:9", nil
-	}
-
-	if math.Abs(ratio-9.0/16.0) < tolerance {
+	} else if height == 16*width/9 {
 		return "9:16", nil
 	}
-
 	return "other", nil
 }
