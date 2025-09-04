0a835d41aefe94f09ac19ee34e98b3de5369f9ac
xixotron <xixotron89@gmail.com>
2025-09-04 21:02:35 +0200

    getObjectURL returns s3 object url with cfg parameters and object 'key'

--- assets.go
+++ assets.go
@@ -35,6 +35,9 @@ func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
 func (cfg apiConfig) getAssetURL(assetPath string) string {
 	return fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetPath)
 }
+func (cfg apiConfig) getObjectURL(key string) string {
+	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
+}
 
 func mediaTypeToExt(mediaType string) string {
 	parts := strings.Split(mediaType, "/")
